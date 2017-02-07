06519ff7bc62a792a3f53a75d3452caf37faec3b
chetanyachopra <chetanya221997chopra@gmail.com>
2017-02-08 00:36:16 +0530

    set he ab sab

--- uber.py
+++ uber.py
@@ -1,6 +1,30 @@
 from uber_rides.session import Session
 from uber_rides.client import UberRidesClient
+import requests
 
+jhaSe=input("jha se= ")
+jhaTak=input("jha tak= ")
+googleMapApiKey='AIzaSyAtx_lIJ0GsFLKtlaCsMyo7K7Rq8IeTCx4'
+googleMapApiResponse=requests.get('https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(jhaSe,googleMapApiKey))
+api_json = googleMapApiResponse.json()
+
+lat_jhase=api_json['results'][0]['geometry']['location']['lat']
+lng_jhase=api_json['results'][0]['geometry']['location']['lng']
+#print('lat= '+str(lat_jhase)+"  "+'lng= '+str(lng_jhase))
+if(api_json['status']=='OK'):
+	
+		googleMapApiResponse_jhatak=requests.get('https://maps.googleapis.com/maps/api/geocode/json?address='+jhaTak+'&key='+googleMapApiKey)
+		api_json_jhatak=googleMapApiResponse_jhatak.json()
+		if(api_json_jhatak['status']=='OK'):
+			lat_jhatak=api_json_jhatak['results'][0]['geometry']['location']['lat']
+			lng_jhatak=api_json_jhatak['results'][0]['geometry']['location']['lng']
+			#print('lat= '+str(lat_jhatak)+"  "+'lng= '+str(lng_jhatak))
+
+		else:
+			print("no cab available here")
+
+else:
+	print('no cab here ')
 serverToken='I799gsdbtW7YqbPSz5y7Tw_5exla3yjBpYmqsQ5L'
 #clientScret='lEU8tHxev2k-gvRAY9X7YU5cG3DSpc8p'
 
@@ -14,10 +38,10 @@ products=response.json.get('products')
 print(products)
 
 '''
-response=client.get_price_estimates( start_latitude=30.733315,
-    start_longitude=76.779418,
-    end_latitude=30.694209,
-    end_longitude=76.860565)
+response=client.get_price_estimates( start_latitude=lat_jhase,
+    start_longitude=lng_jhase,
+    end_latitude=lat_jhatak,
+    end_longitude=lng_jhatak)
 dekhle=response.json.get('prices')
 #distance=estimate[0].get('distance')
 #print(dekhle)
